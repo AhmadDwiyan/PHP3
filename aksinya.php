44127d88d64ff228f38c95dbafdbfcf7905de27c
Nesta2002 <46430576+Nesta2002@users.noreply.github.com>
2019-02-22 10:03:49 +0700

    Add files via upload

---
+++ aksinya.php
@@ -0,0 +1,13 @@
+<?php
+
+$user = $_POST['username'];
+$pw = $_POST['password'];
+
+if($user =="Danza Sasta Vanesta Alsyaluna" && $pw =="baiksekali"){
+echo "Login Berhasil";
+
+} else {
+
+echo" Login Gagal";
+}
+?>
